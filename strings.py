49e23228e32358f6cd11cdf07b7c0f9420daf78e
Tamyrlan <tshurenov@gmail.com>
2022-10-28 13:11:41 +0600

    get token

--- strings.py
+++ strings.py
@@ -1,11 +1,9 @@
 BOT_TOKEN_FILENAME = 'token.txt'
 
-# символы, которые используются
 SYMBOL_X = '❌'
 SYMBOL_O = '⭕'
 SYMBOL_UNDEF = '◻'
 
-# ответы бота
 ANSW_YOUR_TURN = 'Ваш ход'
 ANSW_YOU_WIN = 'Вы победили'
 ANSW_BOT_WIN = 'Победил бот'
@@ -13,5 +11,4 @@ ANSW_DRAW = 'Ничья'
 ANSW_HELP = 'Бот для лёгкой игры в крестики-нолики\n' \
             'Используйте команду /new_game для начала новой игры'
 
-# ошибки
 ALERT_CANNOT_MOVE_TO_THIS_CELL = 'Нажимать можно только на ' + SYMBOL_UNDEF
