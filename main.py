1d0bbcf641a321d70fc427b9fc567f99e0e9ced5
ShuMAhd <shuman@zykaika.ru>
2022-10-28 18:59:06 +0800

    мелкие исправления

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-from email.encoders import encode_noop
-import os
 import random
-import sys
-
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
-
 import strings as st
-
-def get_token():
-    token = ''
-    os.path.isfile(st.BOT_TOKEN_FILENAME)
-    with open(st.BOT_TOKEN_FILENAME, "r") as f:
-        token = f.read()
-    return token
 
 
 def isWin(arr, who):
@@ -28,7 +16,6 @@ def isWin(arr, who):
         return True
     return False
 
-
 def countUndefinedCells(cellArray):
     counter = 0
     for i in cellArray:
@@ -36,19 +23,15 @@ def countUndefinedCells(cellArray):
             counter += 1
     return counter
 
-
-
 def game(callBackData):
-   
-    message = st.ANSW_YOUR_TURN 
+    message = st.ANSW_YOUR_TURN
     alert = None
-
     buttonNumber = int(callBackData[0])
     if not buttonNumber == 9:
-        charList = list(callBackData) 
+        charList = list(callBackData)
         charList.pop(0)
-        if charList[buttonNumber] == st.SYMBOL_UNDEF:  
-            charList[buttonNumber] = st.SYMBOL_X  
+        if charList[buttonNumber] == st.SYMBOL_UNDEF:
+            charList[buttonNumber] = st.SYMBOL_X
             if isWin(charList, st.SYMBOL_X):
                 message = st.ANSW_YOU_WIN
             else:
@@ -58,21 +41,16 @@ def game(callBackData):
                         rand = random.randint(0, 8)
                         if charList[rand] == st.SYMBOL_UNDEF:
                             charList[rand] = st.SYMBOL_O
-                            isCycleContinue = False 
+                            isCycleContinue = False
                             if isWin(charList, st.SYMBOL_O):
                                 message = st.ANSW_BOT_WIN
-
-     
         else:
             alert = st.ALERT_CANNOT_MOVE_TO_THIS_CELL
-
         if countUndefinedCells(charList) == 0 and message == st.ANSW_YOUR_TURN:
             message = st.ANSW_DRAW
-
         callBackData = ''
         for c in charList:
             callBackData += c
-
     if message == st.ANSW_YOU_WIN or message == st.ANSW_BOT_WIN or message == st.ANSW_DRAW:
         message += '\n'
         for i in range(0, 3):
@@ -80,51 +58,42 @@ def game(callBackData):
             for j in range(0, 3):
                 message += callBackData[j + i * 3] + ' | '
         callBackData = None
-
     return message, callBackData, alert
 
-
-
 def getKeyboard(callBackData):
-    keyboard = [[], [], []] 
-    if callBackData != None: 
+    keyboard = [[], [], []]
+    if callBackData != None:
         for i in range(0, 3):
             for j in range(0, 3):
-                keyboard[i].append(InlineKeyboardButton(callBackData[j + i * 3], callback_data=str(j + i * 3) + callBackData))
-
+                keyboard[i].append(
+                    InlineKeyboardButton(callBackData[j + i * 3], callback_data=str(j + i * 3) + callBackData))
     return keyboard
-
 
 def newGame(update, _):
     data = ''
     for i in range(0, 9):
         data += st.SYMBOL_UNDEF
-
-    update.message.reply_text(st.ANSW_YOUR_TURN, reply_markup=InlineKeyboardMarkup(getKeyboard(data)))
-
+    update.message.reply_text(
+        st.ANSW_YOUR_TURN, reply_markup=InlineKeyboardMarkup(getKeyboard(data)))
 
 def button(update, _):
     query = update.callback_query
     callbackData = query.data
-
     message, callbackData, alert = game(callbackData)
     if alert is None:
-        query.answer() 
-        query.edit_message_text(text=message, reply_markup=InlineKeyboardMarkup(getKeyboard(callbackData)))
+        query.answer()
+        query.edit_message_text(
+            text=message, reply_markup=InlineKeyboardMarkup(getKeyboard(callbackData)))
     else:
         query.answer(text=alert, show_alert=True)
-
 
 def help_command(update, _):
     update.message.reply_text(st.ANSW_HELP)
 
-
 if __name__ == '__main__':
-    updater = Updater(get_token()) 
+    updater = Updater('Вставьте сюда свой токен')
     updater.dispatcher.add_handler(CommandHandler('start', newGame))
     updater.dispatcher.add_handler(CommandHandler('new_game', newGame))
     updater.dispatcher.add_handler(CommandHandler('help', help_command))
-    updater.dispatcher.add_handler(MessageHandler(Filters.text, help_command)) 
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
     updater.start_polling()
-    updater.idle()
